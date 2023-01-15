10db49267e8a7edde1211e6d97e8d0e1e40b6eb5
kimdayeon <hamoey@naver.com>
2023-01-15 23:27:57 +0900

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './routes/index.js';
- 
+import { store } from './store/index.js';
+
 const app = createApp(App)
-app.use(router)
+app.use(store).use(router)
 app.mount('#app')
 
 
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import Vuex from 'vuex'; // 상태관리 도구
+
+export const store = new Vuex.Store({
+    state:{
+        news: []
+    },   
+})
