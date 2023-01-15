5c8f6b03d14e09e6ab0c0e85311eca2f8f4d25c0
kimdayeon <hamoey@naver.com>
2023-01-15 23:42:02 +0900

    actions 및 mutations 적용

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,25 @@
 import Vuex from 'vuex'; // 상태관리 도구
+import { fetchNewsList } from '../api/index.js';
 
 export const store = new Vuex.Store({
     state:{
         news: []
     },   
+    mutations:{
+        SET_NEWS(state, news){
+            state.news = news;
+        }
+    },
+    actions:{
+        FETCH_NEWS(context){
+            fetchNewsList()
+                .then(response =>{
+                    console.log(response.data);
+                    context.commit('SET_NEWS', response.data);
+                })
+                .catch(error =>{
+                    console.log(error);
+                });
+        }
+    }
 })
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-for="user in users">{{ user.title }}</div>
+    <div v-for="user in this.$store.state.news">{{ user.title }}</div>
   </div>
 </template>
 
@@ -8,18 +8,15 @@
 import { fetchNewsList } from '../api/index.js';
 
 export default {
-  data(){
-    return{
-      users:[]
-    }
-  },
   created(){
-    fetchNewsList()
-      .then(response => {
-        console.log(response);
-        this.users = response.data;
-      })
-      .catch(error => console.log(error))
+    this.$store.dispatch('FETCH_NEWS');
+
+    // fetchNewsList()
+    //   .then(response => {
+    //     console.log(response);
+    //     this.users = response.data;
+    //   })
+    //   .catch(error => console.log(error))
   },
 }
 </script>
